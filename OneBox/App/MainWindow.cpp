148976087fa13a827347358061faf8a4e6306b03
caijun <caijun@nj-linac.com>
2021-09-17 16:27:00 +0800

    Update to support Qt5.12 build

--- OneBox/App/MainWindow.cpp
+++ OneBox/App/MainWindow.cpp
@@ -34,7 +34,7 @@ MainWindow::MainWindow(QWidget *parent) :
     this->setWindowTitle(tr("All in One ToolBox"));
 
     // Set Menu Bar Version Info
-    ui->menuVersion->addAction("V1.0 2021-Jul-09");
+    ui->menuVersion->addAction("V1.0 2021-Sep-17");
 }
 
 MainWindow::~MainWindow()
--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 A toolbox software provides serial port, TCP server/client, UDP server/client functions based on Qt4.
 
 
+V1.0 2021-Sep-17
+1. Update to support Qt5 build
+
+
 V1.0 2021-Jul-09
 1. Initial release.
 2. Tool support TCP/UDP server client, serial port.
